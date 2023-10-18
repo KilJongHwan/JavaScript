429a50b7247c2d7520c279d1e15c0b0182a39204
JH <dafr47@naver.com>
2023-10-18 17:50:19 +0900

    array 객체 추가

--- src/innerObject.js
+++ src/innerObject.js
@@ -45,5 +45,20 @@ console.log(str3.charCodeAt(0));
 
 let birthday = "1997-06-12";
 let arr2 = birthday.split("-"); // 하이픈(-)을 기준으로 문자열을 분리해서 배열로 
-console.log(arr2); // ["1997","06","02"]
+// console.log(arr2); // ["1997","06","02"]
+
+// Number 객체 : 숫자와 관련된 작업을 할 때 사용하는 메소드
+// Number.parseFloat() 메소드
+
+console.log(Number.parseFloat("12")) ; // 12
+console.log(Number.parseFloat("12.34")); // 12.34
+console.log(Number.parseFloat("12문자열"));  // 12
+console.log(Number.parseFloat("12 34 56")); // 12
+console.log(Number.parseFloat("문자열 56")); // NaN
+
+Number.parseInt("12");         // 12
+Number.parseInt("12.34");      // 12
+Number.parseInt("12문자열");   // 12
+Number.parseInt("12 34 56");   // 12
+Number.parseInt("문자열 56"); // NaN
 
