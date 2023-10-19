8735cc5cd1859c48109cd0cf5024d2e3abb964ad
JH <dafr47@naver.com>
2023-10-19 15:31:07 +0900

    dom 문제 추가

---
+++ src/todo.js
@@ -0,0 +1,16 @@
+const subjectInput = document.getElementById('subjectInput');
+const addButton = document.getElementById('addButton');
+const subjectList = document.getElementById('subjectList');
+
+addButton.onclick = function() {
+    const subjectText = subjectInput.value;
+    if (subjectText) {
+        const listItem = document.createElement('li');
+        listItem.textContent = subjectText;
+        listItem.onclick = function() {
+            listItem.remove();
+        };
+        subjectList.appendChild(listItem);
+        subjectInput.value = '';
+    }
+};
